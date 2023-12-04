b3d530cc4e3b729212b17f251b3fb6c0aecb3021
Kartik Narang <kartiknarang@gmail.com>
2023-12-03 19:06:43 -0500

    Final model prediction file with model files

--- predict.py
+++ predict.py
@@ -1,23 +1,38 @@
 from flask import Flask, request, jsonify
 import joblib
+import numpy as np
 
 app = Flask(__name__)
 
-rf_delay_classifier = joblib.load('rf_delay_classifier_model.pkl')
-rf_delay_regression = joblib.load('rf_delay_regression_model.pkl')
+rf_delay_classifier = joblib.load('rf_delay_binary_model.pkl')
+rf_delay_bucket = joblib.load('rf_delay_bucket_model.pkl')
 
 @app.route('/predict_delay', methods=['POST'])
 def predict_delay():
     try:
-        data = request.json['features']
-        delay_prediction = rf_delay_classifier.predict([data])[0]
+        print("hello")
+        data = request.json
 
-        if delay_prediction == 0:
-            return jsonify({'prediction': 'No Delay'})
+        features_in_order = ['month', 'day', 'day_of_week',
+                              'unique_carrier_id', 'flight_number', 'origin_airport', 'destination_airport',
+                              'expected_departure_time', 'expected_arrival_time',
+                              'awnd_x', 'prcp_x', 'snow_x', 'snwd_x', 'tavg_x', 'wdf2_x',
+                              'lat_x', 'long_x',
+                              'awnd_y', 'prcp_y', 'snow_y', 'snwd_y', 'tavg_y', 'wdf2_y',
+                              'lat_y', 'long_y']
 
-        delay_duration_prediction = rf_delay_regression.predict([data])[0]
+        features_values = [data[feature] for feature in features_in_order]
 
-        return jsonify({'prediction': f'Delay of {delay_duration_prediction} minutes'})
+        # First model
+        delay_prediction_prob = rf_delay_classifier.predict_proba([features_values])
+        delay_probability = delay_prediction_prob[0][0]
+
+        # Second model
+        delay_duration_prediction = rf_delay_bucket.predict_proba([features_values]).ravel()
+        
+        return jsonify({'Probability of No Delay': delay_probability,
+                        'Probability Buckets': list(delay_duration_prediction)})
+
 
     except Exception as e:
         return jsonify({'error': str(e)})
